3c0a78cf7db5f23dea25624c14b9fc537fa33659
g03mk012w <g03mk012w@gmail.com>
2017-11-10 10:58:09 +0900

    初版

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Hex0x
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent() {
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(25, 29);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(432, 23);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(80, 69);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(323, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "0x00, → 00";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(25, 116);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.Size = new System.Drawing.Size(432, 23);
+			this.textBox2.TabIndex = 0;
+			this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(25, 211);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(432, 23);
+			this.textBox3.TabIndex = 0;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(80, 252);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(323, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "00 → 0x00,";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(25, 297);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(432, 23);
+			this.textBox4.TabIndex = 0;
+			this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(481, 358);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox1);
+			this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox4;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Hex0x
+{
+	public partial class Form1 : Form
+	{
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			string out_text = "";
+			string buffer;
+
+			try {
+				string[] bin_data = textBox1.Text.Split(',');
+
+				foreach(var data in bin_data) {
+					buffer = data;
+					buffer = buffer.Trim();
+					buffer = buffer.Replace("0x", "");
+					out_text += buffer + " ";
+				}
+			} catch(Exception ex) {
+				MessageBox.Show(ex.ToString());
+			}
+
+			out_text = out_text.Trim();
+			textBox2.Text = out_text;
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			string out_text = "";
+
+			try {
+				string[] bin_data = textBox3.Text.Split(' ');
+
+				foreach(var data in bin_data) {
+					out_text += "0x" + data + ", ";
+				}
+			} catch(Exception ex) {
+				MessageBox.Show(ex.ToString());
+			}
+
+			out_text = out_text.Trim();
+			out_text = out_text.Trim(',');
+			textBox4.Text = out_text;
+		}
+
+		void textbox_SelectAll(TextBox textbox, KeyEventArgs e) {
+			if(e.Control && (e.KeyCode == Keys.A)) {
+				textbox.SelectAll();
+			}
+		}
+
+		private void textBox2_KeyDown(object sender, KeyEventArgs e) {
+			textbox_SelectAll((TextBox)sender, e);
+		}
+
+		private void textBox4_KeyDown(object sender, KeyEventArgs e) {
+			textbox_SelectAll((TextBox)sender, e);
+		}
+	}
+}
